b0cc2f720af6ac4383597e22e73b3146e10622bf
Alex <alessandro.piccione.75@gmail.com>
2022-10-02 15:28:34 +0100

    load rule from config

--- cleanup.js
+++ cleanup.js
@@ -10,13 +10,23 @@
 //  .then(response => response.json()) // file contains json
 //  .then(json => console.log(json))
 
-const log = (msg, arg) => console.log(`[No-Cookies] > ${msg}`, arg)
+const log = (msg, arg) => console.log(`[No-Cookies] ${msg}`, arg)
 
+/*
 let config = {}
 const data = chrome.storage.sync.get(["config"], result => {
   log("got config from storage:", result.config)
   config = result.config
 })
+*/
+
+const getConfig = async () =>
+  new Promise((resolve, reject) => {
+    chrome.storage.sync.get(["config"], result => {
+      if (result.config) resolve(result.config)
+      else reject()
+    })
+  })
 
 const removeElement = (site, query) => {
   const element = document.querySelector(query)
@@ -25,16 +35,32 @@ const removeElement = (site, query) => {
     log(`Cannot remove element "${query}" because cannot find it in ${site}.`)
 }
 
-const cleanIt = async site => {
-  log(`CleanIt start for ${site}...`)
+const cleanIt = config => {
+  const siteUrl = window?.location?.hostname
+  log(`CleanIt start for ${siteUrl}...`)
   // cannot access to chrome.tabs in content script
 
-  //log("config.query", config?.query)
+  const site = config.sites.find(s => s.url === siteUrl)
 
-  let query = "div.fc-consent-root"
-  removeElement(site, query)
+  if (site.rules) {
+    site.rules.forEach(rule => {
+      if (rule.type == "remove_element") {
+        removeElement(siteUrl, rule.querySelector)
+      }
+    })
+  }
 }
 
-const site = window?.location?.href
+//log("Get config from storage...")
 
-cleanIt(site)
+//log("Got config from storage", config)
+
+/*
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done!"), 6000)
+})
+*/
+
+getConfig()
+  .then(config => cleanIt(config))
+  .catch(err => console.error(`Failed to get config. ${err}`))
--- config.js
+++ config.js
@@ -1,10 +1,18 @@
+const rules = ["remove_element"]
+
 export default {
   version: "1",
   query: "aaaa",
   sites: [
     {
       url: "www.geeksforgeeks.org",
-      rules: [],
+      rules: [
+        {
+          description: 'Remove element <div class="fc-consent-root">',
+          type: "remove_element",
+          querySelector: "div.fc-consent-root",
+        },
+      ],
     },
     {
       url: "google.com",
