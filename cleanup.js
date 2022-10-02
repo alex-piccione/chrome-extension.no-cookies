d510008c1a383179ce564aacbe79566f755480c4
Alex <alessandro.piccione.75@gmail.com>
2022-10-02 11:16:42 +0100

    cleanup

--- cleanup.js
+++ cleanup.js
@@ -1,19 +1,25 @@
+// doesn't work: require is not defined
+//const configFile = require("./wesites.config.json")
+//console.log(configFile)
+
+//const remote_url = "https://github.com/alex-piccione/chrome-extension.no-cookies/blob/feature/add-basic-stuff/wesites.config.json"
+
+//url = chrome.runtime.getURL("./wesites.config.json")
+
+//fetch(url)
+//  .then(response => response.json()) // file contains json
+//  .then(json => console.log(json))
+
 const removeElement = query => {
   const element = document.querySelector(query)
   if (element) element.remove()
   else console.error(`Cannot remove element "${query}" because cannot find it.`)
-  //document.querySelector("div.fc-consent-root").remove()
 }
 
-/*function removeElement(query) {
-      const element = document.querySelector(query)
-      if (element) element.remove()
-      else console.error(`Cannot remove element "${query}" because cannot find it.`)
-      //document.querySelector("div.fc-consent-root").remove()
-      }*/
-
-const cleanIt = () => {
+const cleanIt = async () => {
   console.log("CleanIt start...")
+  // cannot access to chrome.tabs in content script
+
   let query = "div.fc-consent-root"
   removeElement(query)
 }
