914d78b121352b9b5885b4d214fb96c566f309f0
Alex <alessandro.piccione.75@gmail.com>
2022-10-02 09:46:17 +0100

    run script on background

--- background.js
+++ background.js
@@ -1,6 +1,32 @@
-let website = '#3aa757';
+let website = "#3aa757"
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ website });
-  console.log('Default background color set to %cgreen', `color: ${website}`);
+  console.log("onInstalled")
+  //chrome.storage.sync.set({ website })
+  //console.log("Default background color set to %cgreen", `color: ${website}`)
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log("tab.onUpdated")
+
+  if (tab.active && changeInfo.status === "complete") {
+    /*chrome.tabs.executeScript({
+      code: "console.log('dsff');",
+    })*/
+    console.log("tab.onUpdated - execute script")
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      //func: logic.cleanIt, cannot referene object external to the passed function
+      // you can pass args
+      files: ["cleanup.js"],
+    })
+  }
+})
+
+/*
+chrome.tabs.executeScript(tab.id, { code: "console.log('dsff');" }, function() {
+  if (chrome.runtime.lastError) {
+       console.log("ERROR: " + chrome.runtime.lastError.message);
+  }
 });
+*/
