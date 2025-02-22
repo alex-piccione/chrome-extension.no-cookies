9fd1f96c8831e2eec521cc6c314982ec73136f11
Alex <alessandro.piccione.75@gmail.com>
2025-02-22 14:38:44 +0000

    remove duplications

--- data/websites_actions.ts
+++ data/websites_actions.ts
@@ -4,9 +4,11 @@ export default {
     {
       url: "<any>",
       actions: [
-        { remove_element: "div.fc-consent-root", excluded_sites: []},
-        { remove_element: "div#onetrust-consent-sdk", repeat: "5 times, every 500 ms" },
-        { restore_scrolling: null }
+        {remove_element: "div.fc-consent-root", excluded_sites: []},
+        {remove_element: "div#onetrust-consent-sdk", repeat: "15 times, every 500 ms"},
+        {remove_element: "div#ez-cmpv2-container", repeat: "15 times, every 500 ms"},
+        {remove_element: "div#qc-cmp2-container", repeat: "15 times, every 500 ms"},
+        {restore_scrolling: null }
       ]      
     },
     {
@@ -45,16 +47,8 @@ export default {
       actions: [{ remove_element: "div#ez-cookie-dialog-wrapper" }],
     },
     {
-      url: "forums.androidcentral.com",
-      actions: [{ remove_element: "div#qc-cmp2-container" }],
-    },
-    {
       url: "hsbc.co.uk",
       actions: [{ remove_element: "div#__tealiumGDPRecModal", repeat: "2 times, every 300 ms" }],
-    },
-    {
-      url: "sitepoint.com",
-      actions: [{ remove_element: "div#qc-cmp2-container" }],
     },
     {
       url: "appdividend.com",
@@ -114,13 +108,12 @@ export default {
     {
       url: "tomshardware.com",
       actions: [
-        { remove_element: "div#qc-cmp2-container" },
+
         { remove_element: "div#slice-container-newsletterForm-exitIntent" }],
     },
     {
       url: "techradar.com",
-      actions: [        
-        { remove_element: "div#qc-cmp2-container" }, 
+      actions: [
         { remove_element: "div#slice-container-newsletterForm-exitIntent" },
         { remove_element: ".sp_message_container_808540", repeat: "3 times, every 1500 ms" },
         { remove_element: "div[id^=sp_message_container_]", repeat: "5 times, every 500 ms"}]
@@ -132,10 +125,6 @@ export default {
     {
       url: "csharp.hotexamples.com",
       actions: [{ remove_element: "div#ez-cookie-dialog-wrapper"}]
-    },
-    {
-      urL: "baeldung.com",
-      actions: [{ remove_element: "div#qc-cmp2-container", repeat: "3 times, every 500 ms",}]
     },
     {
       url: "thewindowsclub.com",
@@ -156,10 +145,6 @@ export default {
     {
       url: "linuxize.com",
       actions: [{ remove_element: "div#ez-cookie-dialog-wrapper", repeat: "3 times, every 500 ms" }]
-    },
-    {
-      url: "ukradiolive.com",
-      actions: [{remove_element: "div#qc-cmp2-container", repeat: "10 times, every 1500 ms"}]
     },
     {
       url: "letuelezioni.it",
@@ -187,18 +172,6 @@ export default {
       ]
     },
     {
-      url: "csharp.hotexamples.com",
-      actions: [{ remove_element: "div#ez-cmpv2-container"}]
-    },
-    {
-      url: "oraridiapertura24.it",
-      actions: [{remove_element: "div#qc-cmp2-container"}]
-    },
-    {
-      url: "codeigo.com",
-      actions: [{remove_element: "div#ez-cmpv2-container"}]
-    },
-    {
       url: "autoscout24.it",
       actions: [{remove_element: "div#as24-cmp-popup", repeat: "3 times, every 500 ms"}]
     },
@@ -218,14 +191,6 @@ export default {
       actions: [{remove_element: "div.fc-consent-root"}]
     },
     {
-      url: "dotnettutorials.net",
-      actions: [{remove_element: "div#ez-cmpv2-container", repeat: "5 times, every 500 ms"}]
-    },
-    {
-      url: "advrider.com",
-      actions: [{ remove_element: "div#qc-cmp2-container", repeat: "10 times, every 500 ms" }]
-    },
-    {
       url: "geeksforgeeks.org",
       actions: [{remove_element: "div.fc-consent-root", repeat: "5 times, every 500 ms"}]
     },
@@ -236,10 +201,6 @@ export default {
     {
       url: "dnschecker.org",
       actions: [{remove_element: "div#sp_message_container_1117104", repeat: "5 times, every 500 ms"}]
-    },
-    {
-      url: "timeanddate.com",
-      actions: [{ remove_element: "div#qc-cmp2-container", repeat: "5 times, every 500 ms"}]
     },
     {
       url: "ricette.giallozafferano.it", actions: [
