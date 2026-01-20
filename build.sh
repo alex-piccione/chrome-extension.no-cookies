6205b7964e26fc1d14d680be057c260335272908
Alex <alessandro.piccione.75@gmail.com>
2026-01-20 14:19:04 +0100

    clean up

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ npm run build
 
 # prepare required files
 cp -r manifest.json dist/
+sed -i 's/__VERSION__/0.0.0/g' dist/manifest.json
 mkdir -p dist/pages && cp -r pages/* dist/pages/
 mkdir -p dist/images && cp -r images/* dist/images/
